fc65e69cc37def2f361ff58bf0ca8b9fa051f2d6
Marmik Shah <marmikshah@icloud.com>
2020-06-11 12:14:00 +0800

    Added AU Route

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://qlicky.github.io/precache-manifest.a412729b14caecae31d45de026e154ec.js"
+  "https://qlicky.github.io/precache-manifest.4cdda83744be5e4bad5297a5b7f1a9c2.js"
 );
 
 self.addEventListener('message', (event) => {
